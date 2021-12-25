e33d7e620ada002f1da5332643a30fe4faa10155
BouzidYoussef <bouzidyoussef600@yahoo.com>
2021-12-25 14:18:06 +0100

    create react app

---
+++ front/src/App.js
@@ -0,0 +1,122 @@
+
+function App() {
+  return (
+    <div className="grid-container">
+        <header className="row">
+            <div>
+                <a className="brand" href="index.html">Shooper</a>
+            </div>
+            <div>
+                <a href="cart.html">Cart</a>
+                <a href="signin.html">Sign In</a>
+            </div>
+        </header>
+        <main>
+            <div className="row center">
+                <div className="card">
+                    <a href="product.html"><img className="meduim" src="./images/product-1.jpg" alt="product"/></a>
+                    <div className="card-body">
+                        <a href="product.html"><h2>Dancing pyjama</h2></a>
+                        <div className="rating">
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                        </div>
+                        <div className="price">
+                            $70
+                        </div>
+                    </div>
+                </div>
+                <div className="card">
+
+                    <a href="product.html"><img className="meduim" src="./images/product-1.jpg" alt="product"/></a>
+                    <div className="card-body">
+                        <a href="product.html"><h2>Dancing pyjama</h2></a>
+                        <div className="rating">
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                        </div>
+                        <div className="price">
+                            $70
+                        </div>
+                    </div>
+                </div>
+                <div className="card">
+                    <a href="product.html"><img className="meduim" src="./images/product-1.jpg" alt="product"/></a>
+                    <div className="card-body">
+                        <a href="product.html"><h2>Dancing pyjama</h2></a>
+                        <div className="rating">
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                        </div>
+                        <div className="price">
+                            $70
+                        </div>
+                    </div>
+                </div>
+                <div className="card">
+                    <a href="product.html"><img className="meduim" src="./images/product-1.jpg" alt="product"/></a>
+                    <div className="card-body">
+                        <a href="product.html"><h2>Dancing pyjama</h2></a>
+                        <div className="rating">
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                        </div>
+                        <div className="price">
+                            $70
+                        </div>
+                    </div>
+                </div>
+                <div className="card">
+                     <a href="product.html"><img className="meduim" src="./images/product-1.jpg" alt="product"/></a>
+                    <div className="card-body">
+                        <a href="product.html"><h2>Dancing pyjama</h2></a>
+                        <div className="rating">
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                        </div>
+                        <div className="price">
+                            $70
+                        </div>
+                    </div>
+                </div>
+                <div className="card">
+                    <a href="product.html"><img className="meduim" src="./images/product-1.jpg" alt="product"/></a>
+                    <div className="card-body">
+                        <a href="product.html"><h2>Dancing pyjama</h2></a>
+                        <div className="rating">
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                            <span> <i className="fa fa-star"></i> </span>
+                        </div>
+                        <div className="price">
+                            $70
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <footer className="row center">
+            All rights are reserved
+        </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/data.js
@@ -0,0 +1,14 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name:"nike",
+            image:"/images/product-1.jpg",
+            price:70,
+            brand:"Nike",
+            rating:4.5,
+            numReviews:10,
+            description:"High quality shoes",
+        }
+    ]
+}
